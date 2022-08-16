4b92b039c721711bf6871e9437d6099fb975d03b
xeGi524 <sergej.kostic04@gmail.com>
2022-08-16 12:17:55 +0200

    Spremenjen main
    V main.jsx dodan Card  kot komponenta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Card from './Card'
 
 function App() {
   const [count, setCount] = useState(0)
