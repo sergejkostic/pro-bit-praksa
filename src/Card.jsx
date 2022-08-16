59962ca56efbde2f4dcefd6d945132fd5d506bd3
xeGi524 <sergej.kostic04@gmail.com>
2022-08-16 14:01:33 +0200

    Vse razen slike
    Vse dodano in spremenjeno razen slike

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,12 +5,12 @@ function Card() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="card">
+    <div className='card'>
       <div className='picture-cover'>
-        <img src='grass.jpg' className='image'/>
+        <img src='grass.jpg' className='image' />
       </div>
 
-      <div className='profile-image'>
+      <div className='profile-image-cover'>
       </div>
 
       <div className='card-info'>
@@ -18,14 +18,20 @@ function Card() {
         <p>Sem profesionalni programer</p>
       </div>
 
-      <div className='user-info'>
-
+      <div className='followings'>
+        <div className='followers-info'>
+          <p className='text'>Followers</p>
+          <p className='text'>150000</p>
+        </div>
+        <div className='followers-info-2'>
+          <p className='text'>Following</p>
+          <p className='text'>150000</p>
+        </div>
       </div>
 
       <div className='bottom-button'>
-
+        <p className='follow-text'>Follow</p>
       </div>
-
     </div>
   )
 }
