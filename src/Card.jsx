27fc8c87ba89a39cd6bd0b331a91e77334c5f2fa
xeGi524 <sergej.kostic04@gmail.com>
2022-08-16 13:03:29 +0200

    oblika kartice urejena

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,13 +5,24 @@ function Card() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="Card">
-      <div className="Cover">
-        <img src="grass.jpg"/>
+    <div className="card">
+      <div className='picture-cover'>
+        <img src='grass.jpg' className='image'/>
       </div>
-      <div>
-        <p1>Ziga Jaka Gobec</p1>
+
+      <div className='card-info'>
+        <h1>Ziga Gobec</h1>
+        <p>Sem bivsi igralec CS:GO in se trenutno prijavljam na EPICENTER 21</p>
       </div>
+
+      <div className='user-info'>
+
+      </div>
+
+      <div className='button'>
+
+      </div>
+
     </div>
   )
 }
