cd36683fd3e504303dc36359f8138f080df95475
xeGi524 <sergej.kostic04@gmail.com>
2022-08-18 12:06:19 +0200

    dropdown menu added

--- src/Components/Sections/Navbar.jsx
+++ src/Components/Sections/Navbar.jsx
@@ -9,21 +9,35 @@ function Navbar(props) {
                 <img src='yelpLogo.png'  className='logo'/>
             </div>
 
-            <div className='Search_bar_dropdown_menu_holder'>
+            <div className='Search_bar_dropdown_menu_holder'> 
                 <div className='Search_bar'>
-                    <div className='Search_bar_food'></div>
-                    <div className='Search_bar_food_location'></div>
-                    <div className='Search_bar_search_button'></div>
+                    <div className='Search_bar_food'>
+                        <input placeholder='pizza, pub...'></input>
+                    </div>
+                    <div className='Search_bar_food_location'>
+                        <input placeholder='Dublin...'></input>
+                    </div>
+                    <div className='Search_bar_search_button'>
+
+                    </div>
                 </div>
 
-                <div className='Dropdown_menu_holder'></div>
+
+                <div className='Dropdown_menu_holder'>
                 <div className='Dropdown_menu'>
-                    <span className='Dropdown_menu_span'></span>
+                    <span className='Dropdown_menu_span'>Restaurants</span>
                 </div>
-                <div className='Dropdown_menu'></div>
-                <div className='Dropdown_menu'></div>
-                <div className='Dropdown_menu'></div>
-            </div>
+                <div className='Dropdown_menu'>
+                    <span className='Dropdown_menu_span'>Home services</span>
+                </div>
+                <div className='Dropdown_menu'>
+                    <span className='Dropdown_menu_span'>Auto services</span>
+                </div>
+                <div className='Dropdown_menu'>
+                    <span className='Dropdown_menu_span'>More</span>
+                </div>
+                </div>
+            </div> 
         </div>
       )
 }
