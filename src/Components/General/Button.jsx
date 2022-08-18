4a7f226184fa14902ceed2af411d2fe74a5180c6
xeGi524 <sergej.kostic04@gmail.com>
2022-08-18 22:31:25 +0200

    CSS updates

--- src/Components/General/Button.jsx
+++ src/Components/General/Button.jsx
@@ -3,14 +3,7 @@ import './Button.css'
 
 
 function Button(props) {
-
-    if (props.type === "posebni_trans") {
-        return <button className="LIButton">{props.text}</button>
-    } else if (props.type === "posebni_red") {
-        return <button className="LIButton_posebni_red">{props.text}</button>
-    } else {
-        return <button className="Button">{props.text}</button>
-    }
+    return <button className={props.class_name || "Button"}>{props.text}</button>
 }
 
 export default Button;
