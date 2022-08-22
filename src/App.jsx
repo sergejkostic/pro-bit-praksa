9c9c5c4f24c963ff408763a534029463698a80a4
xeGi524 <sergej.kostic04@gmail.com>
2022-08-22 10:55:44 +0200

    cards and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,142 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Navbar</a>
+          <form className="d-flex input-group w-auto">
+            <input
+              type="search"
+              className="form-control rounded"
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="search-addon"
+            />
+            <span className="input-group-text border-0" id="search-addon">
+              <i className="fas fa-search"></i>
+            </span>
+          </form>
+        </div>
+      </nav>
+      <div className='main-container'>
+        <div className="row">
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: 45, height: 45 }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: 45, height: 45 }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: 45, height: 45 }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: 45, height: 45 }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: 45, height: 45 }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: 45, height: 45 }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
